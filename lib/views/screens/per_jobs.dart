50527ddc8fc92f541545f3a531fd413fa3f97aae
Naveenseb <naveenspananthanam0705@gmail.com>
2023-06-29 22:44:56 +0530

    .

--- lib/views/screens/per_jobs.dart
+++ lib/views/screens/per_jobs.dart
@@ -9,7 +9,7 @@ class PermanentJobsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Permanent Jobs'),
+          title: Text('Job Informations'),
         ),
         floatingActionButton: Container(
           width: 100,
